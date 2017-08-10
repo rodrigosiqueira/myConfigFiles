bf1b8e2641d677b837ab1392a802eef0a1c99f21
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-08-09 22:30:10 -0300

    Make fancy-beep a little bit more generic

--- roles/bash/files/.config/beep/beep.sh
+++ roles/bash/files/.config/beep/beep.sh
@@ -2,54 +2,68 @@
 
 declare -r sound_path="~/.config/beep/sounds/complete.oga"
 declare -r sound_app="paplay"
-declare -A parameters
 
 function usage()
 {
-	echo "Usage: beep [OPTION]"
-	echo -e "\t-t\tSpecify time to wait (Seconds)"
-	echo -e "\t-r\tTotal of sound repetitions"
-	echo -e "\t-i\tTotal of intervals"
-	exit 0
+  echo "Usage: beep [OPTION]"
+  echo -e "\t-t\tSpecify time to wait (Seconds)"
+  echo -e "\t-r\tTotal of sound repetitions"
+  echo -e "\t-i\tTotal of intervals"
+  exit 0
 }
 
-function control ()
+function beepcontrol ()
 {
-	# If repetition is not defined, the default is 1
-	if [ -z ${parameters["r"]} ]; then
-		parameters["r"]=1
-	fi
-	# If interval is not defined, the default is 1
-	if [ -z ${parameters["i"]} ]; then
-		parameters["i"]=1
-	fi
-	# If time is null, default to 0
-	if [ -z ${parameters["t"]} ]; then
-		parameters["t"]=0
-	fi
+  local -A parameters
+  local -i totalShift=0
+  local OPTIND
+  while getopts "t:r:i:h" option; do
+    case $option in
+      t | r | i)
+        parameters[$option]=$OPTARG
+        (( totalShift += 2 ))
+        ;;
+      h | * | \?)
+        usage
+        ;;
+    esac
+  done
 
-	local interval_of_interactions=$(eval echo {1..${parameters["i"]}})
-	local repetition_sequence=$(eval echo {1..${parameters["r"]}})
-	for interaction in $interval_of_interactions; do
-		for repetition in $repetition_sequence; do
-			sleep ${parameters["t"]}
-			eval $sound_app $sound_path
-		done
-	done
+  # Shift fancy-beeps parameters in case to eval
+  for (( i = 0; i < $totalShift; i++ )); do
+    shift
+  done
+
+  # If repetition is not defined, the default is 1
+  if [ -z ${parameters["r"]} ]; then
+    parameters["r"]=1
+  fi
+  # If interval is not defined, the default is 1
+  if [ -z ${parameters["i"]} ]; then
+    parameters["i"]=1
+  fi
+  # If time is null, default to 0
+  if [ -z ${parameters["t"]} ]; then
+    parameters["t"]=0
+  fi
+
+  if [ $# -ne 0 ]; then
+    eval "$@"
+    eval $sound_app $sound_path
+    exit 0
+  fi
+
+  local interval_of_interactions=$(eval echo {1..${parameters["i"]}})
+  local repetition_sequence=$(eval echo {1..${parameters["r"]}})
+  for interaction in $interval_of_interactions; do
+    for repetition in $repetition_sequence; do
+      sleep ${parameters["t"]}
+      eval $sound_app $sound_path
+    done
+  done
 }
 
-function beep ()
+function fancy-beep ()
 {
-	control &
+  beepcontrol "$@" &
 }
-
-while getopts "t:r:i:h" option; do
-	case $option in
-		t | r | i)
-			parameters[$option]=$OPTARG
-			;;
-		h | * | \?)
-			usage
-			;;
-	esac
-done
