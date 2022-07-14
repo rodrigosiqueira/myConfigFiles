5baa9d67acf20f11407855bacf7a81e6abc3b3ef
Rodrigo Siqueira <siqueirajordao@riseup.net>
2022-07-14 19:46:07 -0400

    Adapt script to also work on Debian
    Signed-off-by: Rodrigo Siqueira <siqueirajordao@riseup.net>

--- git/setup.sh
+++ git/setup.sh
@@ -4,6 +4,11 @@ git config --global user.email 'siqueirajordao@riseup.net'
 git config --global core.editor vim
 
 # Autocomplete
-printf '# Git magic\n' >> ~/.bashrc
-printf 'source /usr/share/git/completion/git-completion.bash\n' >> ~/.bashrc
-printf 'source /usr/share/git/completion/git-prompt.sh\n' >> ~/.bashrc
+if [[ "${TARGET_DISTRO}" == 'arch' ]]; then
+  printf '# Git magic\n' >> ~/.bashrc
+  printf 'source /usr/share/git/completion/git-completion.bash\n' >> ~/.bashrc
+  printf 'source /usr/share/git/completion/git-prompt.sh\n' >> ~/.bashrc
+elif [[ "${TARGET_DISTRO}" == 'debian' ]]; then
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.config/git-completion.bash
+  echo "[ -f ~/.config/git-completion.bash ] && . ~/.config/git-completion.bash" >> ~/.bashrc
+fi
